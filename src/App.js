917f0bbb57d5031d776035bc0df518d7555a0038
Keyur Chaudhari <keyur.chaudhari@capgemini.com>
2020-05-26 13:19:48 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 
 const App = () => (
   <div>
+    <Header />
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
